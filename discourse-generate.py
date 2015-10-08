50af4abfc806815e8e323cfc994a44362676f153
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2015-10-08 03:48:24 -0700

    v1.1, 7 October 2015: Abstract logging function to external module.

--- discourse-generate.py
+++ discourse-generate.py
@@ -20,25 +20,17 @@ import pprint
 
 import pytumblr
 
-# Set up default values
+import patrick_logger # From https://github.com/patrick-brian-mooney/personal-library
 
-#Can explicitly set this above zero to force more detailed output
-verbosity_level = 2
+# Set up default values
+patrick_logger.verbosity_level = 2
+
 
 # Functions
 
-def log_it(message,minimum_level=1):
-    """Add a message to the log if verbosity_level is at least minimum_level.
-    Currently, the log goes to standard output.
-    """
-    if verbosity_level >= 4: # set verbosity to at least 4 to get this message output in the debug log
-        print "\nDEBUGGING: function log_it() called"
-    if verbosity_level >= minimum_level:
-        print(message)
-
 def print_usage():
     """Print the docstring as a usage message to stdout"""
-    log_it("INFO: print_usage() was called")
+    patrick_logger.log_it("INFO: print_usage() was called")
     print(__doc__)
 
 def weighted_probability(the_length):
@@ -61,37 +53,37 @@ normal_tags = ['Irish literature', 'automatically generated text', 'Patrick Moon
 temporary_tags = []
 the_content = ''
 
-log_it('INFO: Constants and variables set up; trying to read content',2)
+patrick_logger.log_it('INFO: Constants and variables set up; trying to read content',2)
 
 try:
     the_file = open(the_content_path)
     the_content = the_file.read()
     the_file.close()
 except IOError:
-    log_it('ERROR: Couldn\'t open, or couldn\'t read, or couldn\'t close, the content file',0)
+    patrick_logger.log_it('ERROR: Couldn\'t open, or couldn\'t read, or couldn\'t close, the content file',0)
     sys.exit(2)
 
 the_maximum_roll = weighted_probability(len(the_content))
 the_dice_roll=random.random()
-log_it('INFO: Length of content is ' + str(len(the_content)) + '\n   and the dice roll was ' + str(the_dice_roll) + '\n   And the score necessary to post at that length is ' + str(the_maximum_roll),2)
+patrick_logger.log_it('INFO: Length of content is ' + str(len(the_content)) + '\n   and the dice roll was ' + str(the_dice_roll) + '\n   And the score necessary to post at that length is ' + str(the_maximum_roll),2)
 if the_dice_roll < the_maximum_roll:
     # Make the request
-    log_it('INFO: Attempting to post the content', 2)
+    patrick_logger.log_it('INFO: Attempting to post the content', 2)
     the_client.create_text(the_blog_name, state="published", slug=the_slug, tags=normal_tags + temporary_tags, title=the_title, tweet=the_title + ' [URL]', body=the_content)
-    log_it('INFO: the_client is: ' + pprint.pformat(the_client),2)
+    patrick_logger.log_it('INFO: the_client is: ' + pprint.pformat(the_client),2)
     # Empty the existing content file.
     try:
-        log_it('INFO: Opening the file for writing',2)
+        patrick_logger.log_it('INFO: Opening the file for writing',2)
         the_file = open(the_content_path,'w')
-        log_it('INFO: About to truncate file; file position is ' + str(the_file.tell()),2)
+        patrick_logger.log_it('INFO: About to truncate file; file position is ' + str(the_file.tell()),2)
         the_file.truncate(0)
-        log_it('INFO: About to close file',2)
+        patrick_logger.log_it('INFO: About to close file',2)
         the_file.close()
     except IOError:
-        log_it('ERROR: unable to fully empty ' + the_content_path,0)
-        log_it('... the_file is' + pprint.pformat(the_file),2)
+        patrick_logger.log_it('ERROR: unable to fully empty ' + the_content_path,0)
+        patrick_logger.log_it('... the_file is' + pprint.pformat(the_file),2)
         
 else:
-    log_it("INFO: Not posting; length of accumulated content is currently " + str(len(the_content)),2)
+    patrick_logger.log_it("INFO: Not posting; length of accumulated content is currently " + str(len(the_content)),2)
     
-log_it('INFO: We\'re done',2)
+patrick_logger.log_it('INFO: We\'re done',2)
